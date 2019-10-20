6af40b3f4c3de059daac8072abb55cbcddf053a8
Tim Poerschke <info@coder-tim.de>
2019-10-20 20:38:03 +0200

    [TASK] Style home page

--- page.php
+++ page.php
@@ -1,8 +1,12 @@
 <?php get_header(); ?>
 		<article class="page">
 			<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-				<h1 class="title"><?php the_title(); ?></h1>
-				<?php the_content(); ?>
+				<div class="title-outer">
+					<h1 class="title"><?php the_title(); ?></h1>
+				</div>
+				<div class="page-content">
+					<?php the_content(); ?>
+				</div>
 			<?php endwhile; endif; ?>
 		</article><!-- article -->
 <?php get_footer(); ?>
